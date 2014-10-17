eafc2d85617c13ef937ef0e242f10980c1986c9f
Eric Stein <toba@des.truct.org>
2014-10-17 18:59:30 -0500

    ugh, this js doesn't work

---
+++ ugh.js
@@ -0,0 +1,15 @@
+var opacity_trolling = function(obj) {$(this).css("opacity", (Math.random() + 2.0) / 3.0);}
+
+var UGHJS_MIN = 3000;
+var UGHJS_MAX = 12000;
+
+var ughjs_init = function() {
+	setTimeout("ughjs_run", UGHJS_MIN + Math.Random() * (UGHJS_MAX - UGHJS_MIN));
+}
+
+var ughjs_run = function() {
+	$('div').each(opacity_trolling);
+	ughjs_init();
+}
+
+ughjs_init();
